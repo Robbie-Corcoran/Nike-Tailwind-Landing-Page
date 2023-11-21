400202d552a5f38e7a9ba40aa501c5f68b3aaac8
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-21 13:27:57 +0100

    Setting up App.tsx

--- tailwind-nike-landing-page/src/App.tsx
+++ tailwind-nike-landing-page/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import * as domain from "domain";
 
-export default function App() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
-}
+const App = () => (
+  <main>
+
+  </main>
+);
+
+export default App;
