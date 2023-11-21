c28052500749b3eaa1ebbe85b52cb4af21d7c181
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-21 14:51:02 +0100

    Nav bar implemented.

--- tailwind-nike-landing-page/src/sections/Hero.tsx
+++ tailwind-nike-landing-page/src/sections/Hero.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+
+        </section>
     );
 };
 
