5a2914b0dfe953b000eaac2cca01a26b0a404f4b
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-24 11:47:39 +0100

    Implemented ShoeCard.tsx

--- tailwind-nike-landing-page/src/components/ShoeCard.tsx
+++ tailwind-nike-landing-page/src/components/ShoeCard.tsx
@@ -9,12 +9,14 @@ const handleClick = () => {
 }
     return (
         <div className={`
-        border - 2 rounded-xl cursor-pointer max-sm:flex-1
-            ${bigShoeImage === imgURL
+        border-2 rounded-xl
+            ${bigShoeImage === imgURL.bigShoe
                 ? 'border-coral-red'
                 : 'border-transparent'}
-                `} onClick={handleClick}>
-            ShoeCard
+                cursor-pointer max-sm:flex-1`} onClick={handleClick}>
+            <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+                <img src={imgURL.thumbnail} alt="shoe collection" width={127} height={103} className="object-contain"/>
+            </div>
         </div>
     );
 };
