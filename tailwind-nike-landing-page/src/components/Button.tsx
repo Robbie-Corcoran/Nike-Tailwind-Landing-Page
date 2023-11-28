882813b31e3239e164b8464a2a54243b8dfe7dac
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-28 13:41:52 +0100

    Implemented SpecialOffer.tsx and began updating props for Button.tsx with conditional statements for styling.

--- tailwind-nike-landing-page/src/components/Button.tsx
+++ tailwind-nike-landing-page/src/components/Button.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor }) => {
     return (
-        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+        ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red rounded-full text-white border-coral-red'}>
             {label}
             {iconURL &&
                  <img src={iconURL} alt="Arrow right icon" className="ml-2 rounded-full w-5 h-5"/>
